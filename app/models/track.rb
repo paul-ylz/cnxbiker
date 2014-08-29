1a346be3d1c91fcf5e2d123d8048b675d7956974
Paul Yeoh <paulyeoh.lz@gmail.com>
2014-08-29 16:55:00 +0700

    working paperclip attachment

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,5 @@
 class Track < ActiveRecord::Base
   belongs_to :route
   has_attached_file :gpx
-  validates_attachment_content_type :gpx, :content_type => /application\/xml/
+  validates_attachment_file_name :gpx, :matches => [/gpx\Z/]
 end
