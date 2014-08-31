f4df9348a2543700c2d74cfc74b7f91f9fc32af6
Paul Yeoh <paulyeoh.lz@gmail.com>
2014-08-31 23:15:50 +0700

    distance calculation from parsed gpx

--- app/models/gpx.rb
+++ app/models/gpx.rb
@@ -5,12 +5,25 @@ class Gpx
   def initialize(file)
     @file = file
     @points = []
+    @distance = 0
     parse_gpx
   end
 
+  def points
+    @points
+  end
+
   def distance
-    # Gives distance in m
-    # accumulate and calculate the distances between each set of lat/long in the array
+    i = 0
+    loop do
+      a = [ @points[i][:lon].to_f, @points[i][:lat].to_f ]
+      b = [ @points[i+1][:lon].to_f, @points[i+1][:lat].to_f ]
+      # puts "a is #{a} and b is #{b}"
+      @distance += distance_between_points(a, b)
+      i += 1
+      break if i >= @points.length - 2
+    end
+    @distance
   end
 
   def total_ascent
@@ -36,4 +49,23 @@ class Gpx
       return child.content if child.node_name == 'ele'
     end
   end
+
+  # http://stackoverflow.com/questions/12966638/rails-how-to-calculate-the-distance-of-two-gps-coordinates-without-having-to-u
+  def distance_between_points a, b
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm         = 6371          # Earth radius in kilometers
+    rm          = rkm * 1000    # Radius in meters
+
+    dlon_rad = (b[1]-a[1]) * rad_per_deg  # Delta, converted to rad
+    dlat_rad = (b[0]-a[0]) * rad_per_deg
+
+    lat1_rad, lon1_rad = a.map! {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = b.map! {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rm * c # Delta in meters
+  end
+
 end
